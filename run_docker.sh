f920d8c5c3176683108de35646a8161f7c9e8648
Matt Turner <matturner@gmail.com>
2017-09-10 14:22:43 +0000

    Normalise debug start scripts

--- run_docker.sh
+++ run_docker.sh
@@ -1,1 +1,1 @@
-docker run --name http-proxy --rm --network service_net --publish 8088:80 http-proxy
+docker run --name http-proxy -d --rm --publish 80:80 --publish 443:443 --network service_net http-proxy
---
+++ run_docker_debug.sh
@@ -0,0 +1,1 @@
+docker run -it --name http-proxy --rm --publish 80:80 --publish 443:443 --network service_net http-proxy
