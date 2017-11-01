2e1969d3c3299070b8a543f38303c767c7e5a885
Matt Turner <matturner@gmail.com>
2017-11-01 17:02:40 +0000

    Pass to phpsysinfo on host machine, rather than another container.

--- run_docker_debug.sh
+++ run_docker_debug.sh
@@ -1,1 +1,1 @@
-docker run -it --name http-proxy --rm --publish 80:80 --publish 443:443 --network service_net http-proxy
+docker run -it --name http-proxy --rm --publish 80:80 --publish 443:443 --network service_net --add-host "host:172.18.0.1" http-proxy
